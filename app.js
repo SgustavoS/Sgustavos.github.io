4501b559777f79faefd1f7123ee7a9fcd94e3dea
SgustavoS <101343007+SgustavoS@users.noreply.github.com>
2023-03-08 23:55:26 -0300

    archivos _08_2023

--- app.js
+++ app.js
@@ -1,40 +1,74 @@
-//variables
-const Datos_principales = document.querySelector('#boton');
-const foto = document.querySelector('#foto');
-const Nombre = document.querySelector('#Nombre');
-const Apellido = document.querySelector('#Apellido');
-const Edad = document.querySelector('#Edad');
-const Ciudad = document.querySelector('#Ciudad');
-const Nacionalidad = document.querySelector('#Nacionalidad');
-const Email = document.querySelector('#Email');
-const Telefono = document.querySelector('#Telefono');
-
-
-
-
 //funciones
-const mostrarUsuario =async () => {
+async function mostrarUsuario() {
 
-   try {
+  try {
 
-   
-     const url = 'https://randomuser.me/api';
-     const respuesta = await fetch(url);
-     const {results} = await respuesta.json();
-     const datos = results[0];
-    foto.src= datos.picture.medium;
-    Nombre.textContent= datos.name.first
-    Apellido.textContent= datos.name.last
-    Edad.textContent= datos.dob.age
-    Ciudad.textContent= datos.location.city
-    Nacionalidad.textContent= datos.location.country
-    Email.textContent= datos.email
-    Telefono.textContent= datos.cell
-   } catch (error) {
+
+    const url = 'https://randomuser.me/api';
+    const respuesta = await fetch(url);
+    const { results } = await respuesta.json();
+    const datos = results[0];
+    foto.src = datos.picture.medium;
+    Nombre.textContent = datos.name.first;
+    Apellido.textContent = datos.name.last;
+    Edad.textContent = datos.dob.age;
+    Ciudad.textContent = datos.location.city;
+    Nacionalidad.textContent = datos.location.country;
+    Email.textContent = datos.email;
+    Telefono.textContent = datos.cell;
+  } catch (error) {
     console.log(error);
- } 
-     
+  }
+
 
 }
 
-document.addEventListener('click' , mostrarUsuario);
+document.addEventListener('DOMContentLoaded', mostrarUsuario);
+
+//formulario
+
+let sendBtn = document.getElementById('send');
+let resetBtn = document.getElementById('reset');
+let form = document.getElementById('form');
+
+
+//refesco estado del formulario
+form.addEventListener('submit', (e)=> {
+  e.preventDefault();
+});
+
+//  reseteo 
+resetBtn.addEventListener('click', (e)=> {
+ let name = documet.getElementById('name');
+ let email = documet.getElementById('email');
+ let message = documet.getElementById('message');
+
+
+
+ //seteo valores
+
+ name.value='';
+ email.value='';
+ message.value='';
+});
+
+
+
+ // envio datos
+ sendBtn.addEventListener('click', (e)=> {
+  let name = documet.getElementById('name');
+  let email = documet.getElementById('email');
+  let message = documet.getElementById('message');
+
+  // valor localstorage
+  name = name.value;
+  localStorage.setItem('name',name);
+
+  email = email.value;
+  localStorage.setItem('email',email);
+
+  message = message.value;
+  localStorage.setItem('message', message);
+
+
+});
